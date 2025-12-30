c913b51f4e592069e9fcb7e96ad940b45de1e159
kevinsantiagoacosta195 <127454902+kevinsantiagoacosta195@users.noreply.github.com>
2025-12-29 20:28:16 -0500

    Simulacion PWM_LASER MOSFET

--- Simulaciones/Pwm_Laser/modelo/ARD__PWM_LASER/ARD__PWM_LASER.ino
+++ Simulaciones/Pwm_Laser/modelo/ARD__PWM_LASER/ARD__PWM_LASER.ino
@@ -1,20 +1,22 @@
-/*
-  Prueba PWM + Transistor + LED + Shunt
-  Pin PWM: D9
-  MCU: Arduino UNO
-*/
-
-
-
-// Valores de duty para pruebas
-//int dutyValues[] = {0, 64, 128, 192, 255};
-// 0% , 25%, 50%, 75%, 100%
-
 void setup() {
   pinMode(9, OUTPUT);
+
+  // ---- TIMER1: PWM a 5 kHz ----
+  TCCR1A = 0;
+  TCCR1B = 0;
+
+  // Fast PWM, TOP = ICR1
+  TCCR1A |= (1 << COM1A1);   // PWM en pin 9
+  TCCR1A |= (1 << WGM11);
+  TCCR1B |= (1 << WGM12) | (1 << WGM13);
+
+  // Prescaler = 8
+  TCCR1B |= (1 << CS11);
+
+  ICR1 = 399;     // TOP → 5 kHz
+  OCR1A = 100;    // ~50% duty
 }
 
-void loop()
- {
-analogWrite(9,255);
+void loop() {
+  // nada aquí
 }
